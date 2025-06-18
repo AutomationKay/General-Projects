70141557f58ce7c8a74424785f41b3e2bd04c55d
AutomationKay <112325655+AutomationKay@users.noreply.github.com>
2025-06-18 17:12:24 -0400

    Configure ALLOWED_HOSTS for production

--- bp_tracker/bp_tracker/settings.py
+++ bp_tracker/bp_tracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['https://bp-tracker-app.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bp-tracker-app.onrender.com', '127.0.0.1']
 
 
 # Application definition
