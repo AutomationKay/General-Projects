e6a8d22cbefbb8fd580a030c02fec9838d5cc60c
Kamaal <automationkay@gmail.com>
2025-07-06 15:27:41 -0400

    Conducting tests on agent training process

--- Sonic_RL_Project/utils/wrapper.py
+++ Sonic_RL_Project/utils/wrapper.py
@@ -1,3 +1,23 @@
 #utils/wrapper.py
 
 from utils.logger import get_logger
+
+
+
+class RewardShapingWrapper(gym.Wrapper):
+    """
+
+    Reward considerations:
+    Rings: +100 per ring collected
+    Kill enemies: +50
+    Die to enemies: -25
+    Ring < 1: -10 per second
+    Time: +1500 - [time]
+    Standing still: -500 per second not moving
+
+
+    """
+    def step(self, action):
+        """
+        """
+        
